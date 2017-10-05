1bc45e2549017b4d29eecd51f1afa261e67b7ca0
ugetEman <ugetnur@gmail.com>
2017-10-05 10:03:11 +0200

    Adding composite table(attendance) and use fluent API

--- GigHub/Models/ApplicationDbContext.cs
+++ GigHub/Models/ApplicationDbContext.cs
@@ -1,5 +1,5 @@
-﻿using System.Data.Entity;
-using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace GigHub.Models
 {
@@ -7,6 +7,7 @@ namespace GigHub.Models
     {
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -16,6 +17,18 @@ namespace GigHub.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        // used fluent API to turn off multiple cascade paths  that is created due to many to many relations
+        // of Gigs and User.
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gig)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
---
+++ GigHub/Models/Attendance.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GigHub.Models
+{
+    // composite model is used inorder to access directly the attendee
+    public class Attendance
+    {
+        public Gig Gig { get; set; }
+
+        [Key]
+        [Column(Order = 1)]
+        public int GigId { get; set; }
+
+        public ApplicationUser Attendee { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        public string AttendeeId { get; set; }
+    }
+}
